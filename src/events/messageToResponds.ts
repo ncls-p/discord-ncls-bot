6e57186ade92555c13ce4d2792e804872c6c91ab
nicolas <nicolas@archipels.io>
2023-03-15 16:54:51 +0100

    feat: discord bot

---
+++ src/events/messageToResponds.ts
@@ -0,0 +1,19 @@
+import { Message } from "discord.js";
+import { config, MessagesInResponse } from "../config";
+
+export const handleMessageToResponds = (message: Message) => {
+    if (message.author.bot || !message.guild) return;
+
+    const content = message.content.toLowerCase();
+    const messagesInResponse = config.messagesInResponse as MessagesInResponse;
+    for (const response in messagesInResponse) {
+        const triggerWords = messagesInResponse[response];
+
+        for (const word of triggerWords) {
+            if (content.includes(word.toLowerCase())) {
+                message.channel.send(response);
+                return;
+            }
+        }
+    }
+};
